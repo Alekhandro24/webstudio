50e0a86d7497d7b06654a832a2f345e8226e90d1
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2022-07-03 20:37:17 +0300

    добавив js

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,13 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-modal-open]"),
-    closeMenuBtn: document.querySelector("[data-modal-close]"),
-    menu: document.querySelector("[data-modal]"),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
     body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
